88e80610a79056900e1305ede2266869bd2ccc9c
Austrid Sifuentes <asifuentess@iesch.org>
2020-10-08 17:48:34 +0200

    first commit

---
+++ Variables.java
@@ -0,0 +1,10 @@
+public class Variables {
+public static void main(String[] args) {
+    int a = 1;
+    int d = 9;
+    a = a + b;
+    b = a - b;
+
+    System.out.println("a = " + a + ", b = " + b);
+    }
+}
